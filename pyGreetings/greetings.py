e3c337b34af18e2d04ec70531d2a23e7d0eaf4b4
vizored <vizoredd@gmail.com>
2023-08-27 20:23:38 +0200

    greeting

---
+++ pyGreetings/greetings.py
@@ -0,0 +1,14 @@
+store_name = "Sarah's a Store"
+print(store_name)
+
+store_name = "Sarah's a Store"
+print(store_name)
+
+hello = "Hello"
+name = "name"
+
+print (hello + name)
+
+greeting = hello + " " + name
+print(greeting) 
+
