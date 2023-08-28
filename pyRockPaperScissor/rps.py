9baf422f1696355625e309c1d99d077a81789b28
vizored <vizoredd@gmail.com>
2023-08-28 18:56:49 +0200

    some random tasks

---
+++ pyRockPaperScissor/rps.py
@@ -0,0 +1,17 @@
+from random import choice
+
+choose = ['rock', 'paper', 'scissors']
+
+computer_choice = choice(choose)
+user_choice = input('Do you want rock, paper or scissors?\n')
+
+if computer_choice == user_choice:
+    print("its a TIE!")
+elif user_choice == 'rock' and computer_choice == 'scissors':
+    print("you WIN!")
+elif user_choice == 'paper' and computer_choice == 'rock':
+    print("you WIN!")
+elif user_choice == 'scissors' and computer_choice == 'paper':
+    print("you WIN!")
+else:
+    print("you have LOST!")
